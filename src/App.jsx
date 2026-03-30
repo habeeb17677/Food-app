35c0fb2ce05bac24b822178b7e636fc943235497
michael <michaeloyenuga4@gmail.com>
2026-03-30 13:41:50 -0700

    create hero.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
 
@@ -5,10 +6,11 @@ function App() {
 
 
   return (
-    // <>
-    //   <h1 className="text-3xl text-[red]">Welcome to New Horizon i love coding</h1>
-    // </>
-    <Navbar />
+    <>
+      <Navbar />
+      <Hero />
+    </>
+    
   )
 }
 
