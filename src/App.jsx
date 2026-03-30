78d63b25eb4d4de40de3c4b4b6d78c4551a1cb75
michael <michaeloyenuga4@gmail.com>
2026-03-30 11:28:11 -0700

    Initial Commit Create Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+
+  return (
+    // <>
+    //   <h1 className="text-3xl text-[red]">Welcome to New Horizon i love coding</h1>
+    // </>
+    <Navbar />
+  )
+}
+
+export default App
